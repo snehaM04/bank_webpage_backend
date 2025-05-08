9fec919a0e3d283a681f389142b9ec8a731ccb48
snehaM04 <snehamurugesan4@gmail.com>
2025-05-08 16:18:10 +0530

    new commit

--- src/main/java/com/example/onlineBankingSystem/SecurityConfig.java
+++ src/main/java/com/example/onlineBankingSystem/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.onlineBankingSystem;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,7 +15,10 @@ public class SecurityConfig {
                 .csrf().disable()
                 .cors().and()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/login", "/api/customer/create" , "/api/account/create" , "/api/transaction/deposit" ,"/api/transaction/transactionHistory").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/login", "/api/customer/create", "/api/account/create",
+                        "/api/transaction/deposit", "/api/transaction/withdraw", "/api/transaction/transfer").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/customer/getDetails", "/api/account/getAccountDetails",
+                        "/api/transaction/transactionHistory").permitAll()
                 .anyRequest().authenticated();
 
         return http.build();
